0e7c53321b764f6fc0dc53ef2d14e2c902a6c742
Phangggggggg <pith.laohavirojana@gmail.com>
2024-07-10 20:21:09 +0700

    add airbyte integration

---
+++ integration/pipelines/northwind_integration_pipeline.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from connectors.airbyte import AirbyteClient
+
+if __name__ == "__main__":
+    load_dotenv()
+    AIRBYTE_USERNAME = os.environ.get("AIRBYTE_USERNAME")
+    AIRBYTE_PASSWORD = os.environ.get("AIRBYTE_PASSWORD")
+    AIRBYTE_SERVER_NAME = os.environ.get("AIRBYTE_SERVER_NAME")
+    AIRBYTE_CONNECTION_ID = os.environ.get("AIRBYTE_CONNECTION_ID")
+    
+    airbyte_client = AirbyteClient(
+        server_name=AIRBYTE_SERVER_NAME,
+        username=AIRBYTE_USERNAME,
+        password=AIRBYTE_PASSWORD
+    )
+    
+    if airbyte_client.valid_connection():
+        airbyte_client.trigger_sync(
+            connection_id=AIRBYTE_CONNECTION_ID
+        )

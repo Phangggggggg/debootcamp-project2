9481003e31abeca3e08c3d427c4a70dfe71f234d
Phangggggggg <pith.laohavirojana@gmail.com>
2024-07-10 22:05:41 +0700

    fix cdc sql

--- integration/sql/cdc.sql
+++ integration/sql/cdc.sql
@@ -19,20 +19,7 @@ ALTER TABLE public.us_states REPLICA IDENTITY DEFAULT;
 
 CREATE PUBLICATION airbyte_publication FOR TABLE categories, customer_customer_demo, customer_demographics, customers, employee_territories, employees, order_details, orders, products, region, shippers, suppliers, territories, us_states;
 
-ALTER PUBLICATION airbyte_publication ADD TABLE categories;
-ALTER PUBLICATION airbyte_publication ADD TABLE customer_customer_demo;
-ALTER PUBLICATION airbyte_publication ADD TABLE customer_demograhics;
-ALTER PUBLICATION airbyte_publication ADD TABLE customers;
-ALTER PUBLICATION airbyte_publication ADD TABLE employee_territories;
-ALTER PUBLICATION airbyte_publication ADD TABLE employees;
-ALTER PUBLICATION airbyte_publication ADD TABLE order_details;
-ALTER PUBLICATION airbyte_publication ADD TABLE orders;
-ALTER PUBLICATION airbyte_publication ADD TABLE products;
-ALTER PUBLICATION airbyte_publication ADD TABLE region;
-ALTER PUBLICATION airbyte_publication ADD TABLE shippers;
-ALTER PUBLICATION airbyte_publication ADD TABLE suppliers;
-ALTER PUBLICATION airbyte_publication ADD TABLE territories;
-ALTER PUBLICATION airbyte_publication ADD TABLE us_state;
+
 
 
 SELECT * FROM PG_PUBLICATION_TABLES;

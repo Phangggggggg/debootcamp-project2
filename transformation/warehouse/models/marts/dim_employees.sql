308de2a6fe65d8e186c655d4cf0e46542cb00ead
goosegofly <168407872+goosegofly@users.noreply.github.com>
2024-07-13 10:27:31 +0800

    Create dim_employees.sql

---
+++ transformation/warehouse/models/marts/dim_employees.sql
@@ -0,0 +1,20 @@
+select
+	t1.employee_id 
+	, t1.last_name
+	, t1.first_name
+	, t1.title
+	, t1.birth_date
+	, t1.hire_date
+	, t1.address
+	, t1.city
+	, t1.region
+	, t1.postal_code
+	, t1.country
+	, t1.home_phone
+	, t3.territory_description
+from 
+{{ref('employees')}} as  t1
+left join 
+{{ref('employee_territories')}} as  t2 on t1.employee_id = t2.employee_id
+left join 
+{{ref('territories')}} as t3	on t2.territory_id= t3.territory_id

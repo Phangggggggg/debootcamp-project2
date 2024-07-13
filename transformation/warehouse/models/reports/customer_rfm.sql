1ae32cff20210792f07d8076c4d482b3332a907c
goosegofly <168407872+goosegofly@users.noreply.github.com>
2024-07-13 11:25:09 +0800

    Create customer_rfm.sql

---
+++ transformation/warehouse/models/reports/customer_rfm.sql
@@ -0,0 +1,11 @@
+select 
+customer_id
+, sum(unit_price*quantity) as total_spend
+, count(distinct t1.order_id) as order_cnt
+, sum(unit_price*quantity) / count(distinct t1.order_id) as avg_order_value 
+, max(order_date) as latest_purchase_at
+from 
+{{ref('orders')}} t1
+left join 
+{{ref('order_details')}} t2 on t1.order_id = t2.order_id 
+group by 1

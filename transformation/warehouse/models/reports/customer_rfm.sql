3282024a83add8b79a720f7dee66b4d74dc04781
goosegofly <168407872+goosegofly@users.noreply.github.com>
2024-07-13 11:34:25 +0800

    Update customer_rfm.sql

--- transformation/warehouse/models/reports/customer_rfm.sql
+++ transformation/warehouse/models/reports/customer_rfm.sql
@@ -1,12 +1,14 @@
 select 
-customer_id
+customer_id as customer_id
 , sum(unit_price*quantity) as total_spend
 , count(distinct t1.order_id) as order_cnt
-, sum(unit_price*quantity) / count(distinct t1.order_id) as avg_order_value 
+, cast(sum(unit_price*quantity) / count(distinct t1.order_id)*100 as int) as avg_order_value 
 , max(order_date) as latest_purchase_at
 from 
-{{ref('orders')}} t1
+orders t1
 left join 
-{{ref('order_details')}} t2 on t1.order_id = t2.order_id 
+order_details t2 on t1.order_id = t2.order_id 
+where 
+	t1.order_date >= '1998-01-01'
 group by 1
 order by 1
